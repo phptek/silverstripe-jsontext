1729fe1db0113e523b7daea9eec939629156eb21
Russell Michell <russ@theruss.com>
2018-04-30 21:32:48 +1200

    FIX: Attempt #N at getting travis config right.

--- tests/fixtures/php/MyAwesomeJSONPage.php
+++ tests/fixtures/php/MyAwesomeJSONPage.php
@@ -4,12 +4,12 @@ namespace PhpTek\JSONText\Dev\Fixture;
 
 use PhpTek\JSONText\ORM\FieldType\JSONText;
 use SilverStripe\Dev\TestOnly;
-use Page;
+use SilverStripe\CMS\Model\SiteTree;
 
 /**
  * @package silverstripe-jsontext
  */
-class MyAwesomeJSONPage extends Page implements TestOnly
+class MyAwesomeJSONPage extends SiteTree implements TestOnly
 {
     private static $db = [
         'MyJSON' => JSONText::class
